a666e9c74cd7a28f2e5c4cd4cab098129a13232b
Manuel Martinez <anymus89hd@gmail.com>
2022-07-01 03:08:02 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Manuel Martinez on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
